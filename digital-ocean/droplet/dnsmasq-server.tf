e9c52a89c16c30a7d033790d976c783bd550a204
Joe Wroe <jwroe@thoughtworks.com>
2020-02-15 15:28:37 +0000

    [DO] dnsmasq - Add initial server config

---
+++ digital-ocean/droplet/dnsmasq-server.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_droplet" "dnsmasq-server" {
+    image 				= "ubuntu-14-04-x64"
+    name 				= "dnsmasq-server"
+    region 				= "lon1"
+    size 				= "s-1vcpu-1gb"
+    private_networking 	= true
+    ssh_keys 			= [
+		"var.ssh_fingerprint"
+    ]
+	connection {
+		user 		= "root"
+		type 		= "ssh"
+		private_key = "file(var.pvt_key)"
+		timeout 	= "2m"
+		host    	= "self.public_ip"
+	}
+	provisioner "remote-exec" {
+		inline = [
+			"export PATH=$PATH:/usr/bin",
+			# install dnsmasq
+			"sudo apt-get update -y",
+			"sudo apt-get install -y dnsmasq"
+		]
+	}
+}
