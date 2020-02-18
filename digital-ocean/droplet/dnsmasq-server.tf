1641a10460078838005b8ea924892cf859c85659
Joe Wroe <jwroe@thoughtworks.com>
2020-02-18 13:14:49 +0000

    [DO] dnsmasq - setup addn-hosts

--- digital-ocean/droplet/dnsmasq-server.tf
+++ digital-ocean/droplet/dnsmasq-server.tf
@@ -14,12 +14,22 @@ resource "digitalocean_droplet" "dnsmasq-server" {
 		timeout 	= "2m"
 		host    	= self.ipv4_address
 	}
+	provisioner "file" {
+		source = "./dnsmasq.conf"
+		destination = "/etc/dnsmasq.conf.new"
+	}
 	provisioner "remote-exec" {
 		inline = [
 			"export PATH=$PATH:/usr/bin",
+
 			# install dnsmasq
 			"sudo apt-get update -y",
-			"sudo apt-get install -y dnsmasq"
+			"sudo apt-get install -y dnsmasq",
+
+			# configure dnsmasq
+			"cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig",
+			"cp /etc/dnsmasq.conf.new /etc/dnsmasq.conf",
+			"rm /etc/dnsmasq.conf.new"
 		]
 	}
 }
